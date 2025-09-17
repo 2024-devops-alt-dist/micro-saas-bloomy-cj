eecca61cd105e0ade38bb5f1700c4375697749b7
Camille Janin <cmille.jn@gmail.com>
2025-09-17 18:48:47 +0200

    feat(*): Endpoint GET /api/health accessible

---
+++ client/src/services/api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const apiUrl = import.meta.env.VITE_API_URL + '/api';
+
+const api = axios.create({
+    baseURL: apiUrl,
+});
+
+export const getHealth = () => api.get('/health');
