edfd1174aa43b95493836cffe387e404fe283f9d
Camille Janin <cmille.jn@gmail.com>
2025-10-13 13:32:32 +0200

    feat: installation et test prettier & lefthook back

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,21 +1,23 @@
-import express, { Application } from 'express';
-import cors from 'cors';
+import express, { Application } from "express";
+import cors from "cors";
 
 const app: Application = express();
 const port = process.env.API_PORT;
 
 // Middleware CORS pour autoriser ton frontend
-app.use(cors({
-    origin: 'http://localhost:5173', // ton frontend Vite
-    methods: ['GET', 'POST']
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173", // ton frontend Vite
+    methods: ["GET", "POST"],
+  })
+);
 
-app.get('/api/health', async (_req, res) => {
-    try {
-        res.status(200).json({ status: 'ok', message: 'API connected to database!' });
-    } catch (error) {
-        res.status(500).json({ status: 'error', message: 'Database connection failed' });
-    }
+app.get("/api/health", async (_req, res) => {
+  try {
+    res.status(200).json({ status: "ok", message: "API connected to database!" });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: "Database connection failed" });
+  }
 });
 
 export default app;
