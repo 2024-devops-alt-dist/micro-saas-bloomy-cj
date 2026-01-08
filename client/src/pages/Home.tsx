a0699a32d1e7715dada7b64e69e87e53b6c72347
Camille Janin <cmille.jn@gmail.com>
2026-01-08 16:50:02 +0100

    feat: update home page btn to login

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,45 +1,21 @@
 import { CustomButton } from "../features/buttons/button";
 import { Flex } from "@radix-ui/themes";
+import { useNavigate } from "react-router-dom";
 import "../assets/styles/home.css";
-import { useState } from "react";
-import { getHealth } from "../services/api";
 
 export default function Home() {
-  const [result, setResult] = useState<any>(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
-  const handleTestApi = async () => {
-    setLoading(true);
-    setError(null);
-    setResult(null);
-    try {
-      const response = await getHealth();
-      setResult(response.data);
-    } catch (err: any) {
-      setError("Impossible de joindre l’API");
-    } finally {
-      setLoading(false);
-    }
+  const handleLoginRedirect = () => {
+    navigate("/login"); 
   };
+
   return (
-    <Flex align="center" justify="center" className="home-bg">
+    <Flex align="center" justify="center" className="home-bg" style={{ height: "100vh" }}>
       <Flex direction="column" align="center" justify="center" gap="4" className="home-card">
-        <h1>Tester c'est douter :</h1>
-        <CustomButton onClick={handleTestApi} disabled={loading}>
-          {loading ? "Test en cours..." : "Tester la connexion"}
+        <CustomButton onClick={handleLoginRedirect}>
+          Se connecter / S'inscrire
         </CustomButton>
-        {result && (
-          <div className="mt-4 bg-gray-100 p-4 rounded text-center">
-            <p>
-              <strong>Statut :</strong> {result.status}
-            </p>
-            <p>
-              <strong>Message :</strong> {result.message}
-            </p>
-          </div>
-        )}
-        {error && <div className="text-red-500 mt-2">{error}</div>}
       </Flex>
     </Flex>
   );
